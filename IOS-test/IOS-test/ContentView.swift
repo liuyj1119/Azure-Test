09837bc949dc082ac32cc7de8aae94679577ff02
liuyj1119 <61114172+liuyj1119@users.noreply.github.com>
2021-03-14 11:50:40 +0900

    2021.03.14AppSwift

---
+++ IOS-test/IOS-test/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  IOS-test
+//
+//  Created by YUJUN LIU on 2021/03/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+        }
+    }
+}
---
+++ IOS-test/IOS-test/IOS_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_testApp.swift
+//  IOS-test
+//
+//  Created by YUJUN LIU on 2021/03/14.
+//
+
+import SwiftUI
+
+@main
+struct IOS_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
